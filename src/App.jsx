de4a5ebafe44eee6a3fa4b3dfb6e3e59dc77c511
Paulo Eduardo Dias da Silva <pauloeddias@gmail.com>
2022-09-12 20:19:34 +0000

    Reading if the user has an NFT

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import SelectCharacter from './Components/SelectCharacter';
 import twitterLogo from './assets/twitter-logo.svg';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import myEpicGame from './utils/MyEpicGame.json';
+import { ethers } from 'ethers';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,6 +13,11 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const App = () => {
   // State
   const [currentAccount, setCurrentAccount] = useState(null);
+  
+  /*
+   * Right under current account, setup this new state property
+   */
+  const [characterNFT, setCharacterNFT] = useState(null);
 
   // Actions
   const checkIfWalletIsConnected = async () => {
@@ -36,6 +45,34 @@ const App = () => {
     }
   };
 
+
+  // Render Methods
+  const renderContent = () => {
+    /*
+     * Scenario #1
+     */
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            alt="Monty Python Gif"
+          />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+      /*
+       * Scenario #2
+       */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    }
+  };
   /*
    * Implement your connectWallet method here
    */
@@ -65,45 +102,84 @@ const App = () => {
     }
   };
 
+  const checkNetwork = async () => {
+    try { 
+      if (window.ethereum.networkVersion !== '5') {
+        alert("Please connect to Goerli!")
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
+  useEffect(() => {
+    checkNetwork()
+  }, []);
+
+  /*
+ * Add this useEffect right under the other useEffect where you are calling checkIfWalletIsConnected
+ */
+useEffect(() => {
+  /*
+   * The function we will call that interacts with our smart contract
+   */
+  const fetchNFTMetadata = async () => {
+    console.log('Checking for Character NFT on address:', currentAccount);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    const txn = await gameContract.checkIfUserHasNFT();
+    if (txn.name) {
+      console.log('User has character NFT');
+      setCharacterNFT(transformCharacterData(txn));
+    } else {
+      console.log('No character NFT found');
+    }
+    
+  };
+
+  /*
+   * We only want to run this, if we have a connected wallet
+   */
+  if (currentAccount) {
+    console.log('CurrentAccount:', currentAccount);
+    fetchNFTMetadata();
+  }
+}, [currentAccount]);
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
-          <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-            {/*
-             * Button that we will use to trigger wallet connect
-             * Don't forget to add the onClick event to call your method!
-             */}
-            <button
-              className="cta-button connect-wallet-button"
-              onClick={connectWalletAction}
-            >
-              Connect Wallet To Get Started
-            </button>
-          </div>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built with @${TWITTER_HANDLE}`}</a>
-        </div>
+  <div className="App">
+    <div className="container">
+      <div className="header-container">
+        <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+        <p className="sub-text">Team up to protect the Metaverse!</p>
+        {/* This is where our button and image code used to be!
+         *	Remember we moved it into the render method.
+         */}
+        {renderContent()}
+      </div>
+      <div className="footer-container">
+        <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <a
+          className="footer-text"
+          href={TWITTER_LINK}
+          target="_blank"
+          rel="noreferrer"
+        >{`built with @${TWITTER_HANDLE}`}</a>
       </div>
     </div>
-  );
-};
+  </div>
+);
+}
 
 export default App;
---
+++ src/constants.jsx
@@ -0,0 +1,16 @@
+const CONTRACT_ADDRESS = '0x5831d9046a077A49bfC078a306E4a80Ca6A87f9a';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
